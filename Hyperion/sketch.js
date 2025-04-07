ddc2655aa78b6d125e9efc16a3595357c2513927
Queen-Emeraldas <71834351+Queen-Emeraldas@users.noreply.github.com>
2025-04-07 21:28:27 +0200

    Feat: Add initial CSS styles for the Sandbox project

--- Hyperion/sketch.js
+++ Hyperion/sketch.js
@@ -1,7 +1,8 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
 }
 
 function draw() {
   background(220);
+  t=0,draw=e=>{for(t||createCanvas(W=720,W),noFill(H=W/2),stroke(255,128),ellipseMode(CENTER),background(0),translate(H,H),rotate(t),a=0;a<1;a+=.01)push(),rotate(.1*a*noise(t)),ellipse(0,0,H*(1+(-.5+noise(a,t))),H/1.618*(1+(-.5+noise(t,a)))),pop();t+=.001};
 }

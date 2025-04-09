47ad07a047972f5b99411a8fb41f6424053afcc2
Queen-Emeraldas <71834351+Queen-Emeraldas@users.noreply.github.com>
2025-04-09 22:09:52 +0200

    Feat: Update setup and draw functions for improved canvas and interaction

--- Hyperion/sketch.js
+++ Hyperion/sketch.js
@@ -1,8 +1,36 @@
+//   ___ ___ ___ ___ _______ _______ _______ ___ _______ ______
+//  |   Y   |   Y   |   _   |   _   |   _   |   |   _   |   _  \
+//  |.  1   |   1   |.  1   |.  1___|.  l   |.  |.  |   |.  |   |
+//  |.  _   |\_   /|.  ____|.  __)|.  _   |.  |.  |   |.  |   |
+//  |:  |   | |:  | |:  |   |:  1   |:  |   |:  |:  1   |:  |   |
+//  |::.|:. | |::.| |::.|   |::.. . |::.|:. |::.|::.. . |::.|   |
+//   ---  ---'  ---'  ---'   ---------- ----------------      ---'
+
+// Setup
+// This function is called once at the beginning of the program
 function setup() {
-  createCanvas(200, 200);
+
+    const BACKGROUND_COLOR = color(20, 29, 34)
+    const STAR_COLOR = color(255, 191, 0)
+    const PLANET_COLOR = color(123, 182, 97)
+    const MOON_COLOR = color(250, 235, 215)
+    const ASTEROID_COLOR = color(132, 132, 130)
+    const ORBIT_COLOR = color(215, 215, 215)
+
+    createCanvas(windowWidth, windowHeight);
+    background(BACKGROUND_COLOR);
 }
 
+// Window Resized
+// This function is called whenever the window is resized
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    background(20, 29, 34);
+}
+
+// Draw
+// This function is called continuously in a loop
 function draw() {
-  background(220);
-  t=0,draw=e=>{for(t||createCanvas(W=720,W),noFill(H=W/2),stroke(255,128),ellipseMode(CENTER),background(0),translate(H,H),rotate(t),a=0;a<1;a+=.01)push(),rotate(.1*a*noise(t)),ellipse(0,0,H*(1+(-.5+noise(a,t))),H/1.618*(1+(-.5+noise(t,a)))),pop();t+=.001};
+    fill(255, 191, 0)
+    circle(width / 2, height / 2, 9);
 }
--- Sandbox/sketch.js
+++ Sandbox/sketch.js
@@ -1,7 +1,26 @@
+// t=0,draw=e=>{for(t||createCanvas(W=720,W),noFill(H=W/2),stroke(255,128),ellipseMode(CENTER),background(0),translate(H,H),rotate(t),a=0;a<1;a+=.01)push(),rotate(.1anoise(t)),ellipse(0,0,H*(1+(-.5+noise(a,t))),H/1.618*(1+(-.5+noise(t,a)))),pop();t+=.001};
+// background(0);stroke(255);fill(0,0);ellipse(width/2,height/2,100,100);line(width/2,height/2,mouseX,mouseY);
+// background(0);stroke(255);translate(width/2,height/2);rotate(frameCount*0.01);for(i=0;i<100;i++){angle=map(i,0,100,0,TWO_PI);x=cos(angle)*200;y=sin(angle)*200;ellipse(x,y,20,20);}
+
+//https://dzr.page.link/jqbMucmTdYEdXxwM8
+//https://dzr.page.link/141vx5zq282BCAVJ6
+//https://dzr.page.link/C2MPbdoKqrk1RE3x6
+
+// Setup function
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(720, 720); // Create a canvas of size 720x720
+    noFill(); // Disable filling shapes
 }
 
+// Draw function
 function draw() {
-  background(220);
+    //when mouse button is pressed, circles turn black
+if (mouseIsPressed === true) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  
+  //white circles drawn at mouse position
+  circle(mouseX, mouseY, 100);
 }

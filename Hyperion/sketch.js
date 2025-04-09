7f524893d9d00014d2e0b36224ede3646ab76b0b
Queen-Emeraldas <71834351+Queen-Emeraldas@users.noreply.github.com>
2025-04-09 22:49:39 +0200

    Feat: Implement calculateOrbit function and add radians method to Math

--- Hyperion/sketch.js
+++ Hyperion/sketch.js
@@ -1,10 +1,30 @@
 //   ___ ___ ___ ___ _______ _______ _______ ___ _______ ______
 //  |   Y   |   Y   |   _   |   _   |   _   |   |   _   |   _  \
 //  |.  1   |   1   |.  1   |.  1___|.  l   |.  |.  |   |.  |   |
-//  |.  _   |\_   /|.  ____|.  __)|.  _   |.  |.  |   |.  |   |
+//  |.  _   |\_   _/|.  ____|.  __)_|.  _   |.  |.  |   |.  |   |
 //  |:  |   | |:  | |:  |   |:  1   |:  |   |:  |:  1   |:  |   |
 //  |::.|:. | |::.| |::.|   |::.. . |::.|:. |::.|::.. . |::.|   |
-//   ---  ---'  ---'  ---'   ---------- ----------------      ---'
+//  `--- ---' `---' `---'   `-------`--- ---`---`-------`--- ---'
+
+function calculateOrbit(orbitCenterX, orbitCenterY, radius, eccentricity) {
+
+    // Begin shape to draw a custom path
+    beginShape();
+    
+    // One point for each degree to create a smooth elliptical orbit
+    for (let angleDeg = 0; angleDeg <= 360; angleDeg++) {
+      const angle = angleDeg * Math.PI / 180;
+      const r = radius * (1 - Math.pow(eccentricity, 2)) / (1 + eccentricity * Math.cos(angle));
+      const x = orbitCenterX + r * Math.cos(angle);
+      const y = orbitCenterY + r * Math.sin(angle);
+      
+      // Add vertex to the shape
+      vertex(x, y);
+    }
+    
+    // Close the shape
+    endShape(CLOSE);
+  }
 
 // Setup
 // This function is called once at the beginning of the program
@@ -31,6 +51,11 @@ function windowResized() {
 // Draw
 // This function is called continuously in a loop
 function draw() {
-    fill(255, 191, 0)
+    fill(255, 191, 0) // Set fill color to yellow
     circle(width / 2, height / 2, 9);
+
+    noFill(); // Disable fill for the orbit
+    stroke(215, 215, 215); // Set stroke color to white
+    strokeWeight(0.05); // Set stroke weight
+    calculateOrbit(width / 2, height / 2, 100, 0)
 }
